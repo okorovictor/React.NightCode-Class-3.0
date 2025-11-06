0c0a45f4bd8b9c3dcdc00b78be44d675e839ad1c
Okoro Victor <okorov800@gmail.com>
2025-11-06 13:36:03 +0100

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,34 @@
 import "./App.css";
-import Button from "./Button";
-import { pupils } from "./pupils";
+import { useState } from "react";
 
 function App() {
-  const isValid = false;
+  const [index, setIndex] = useState(0);
+
+  const handleIncreaseClick = () => {
+    setIndex(index + 1);
+    console.log(index);
+  };
+
+  const handleDecreaseClick = () => {
+    setIndex(index - 1);
+    console.log(index);
+  };
 
   return (
     <main className="grid justify-items-center min-h-screen py-20 gap-5 bg-gray-00">
-      <h1>Welcome to my page</h1>
-
-      {/* Props */}
-
-      <section className="space-x-5">
-        {isValid ? (
-          <Button
-            btnBgColor="bg-red-500"
-            btnText="Log In"
-            handleClick={() => console.log("Login Successfully")}
-          />
-        ) : (
-          <Button
-            btnBgColor="bg-blue-500"
-            btnText="Sign Up"
-            handleClick={() => console.log("Signin Successfully")}
-          />
-        )}
-
-        <Button
-          btnBgColor="bg-green-500"
-          btnText="Get STarted"
-          handleClick={() => console.log("Get Started Successfully")}
-        />
-        <Button
-          btnText="Explore More"
-          handleClick={() => alert("Visit my X(twitter) handle")}
-        />
-      </section>
-
-      {/* List Rendering */}
-
-      <ol>
-        {pupils.map((pupil) => (
-          <li key={pupil.id}>{pupil.name}</li>
-        ))}
-      </ol>
+      <h1 className="text-4xl">{index}</h1>
+      <button
+        className="bg-blue-500 p-3 text-white cursor-pointer rounded-2xl hover:bg-blue-700"
+        onClick={handleIncreaseClick}
+      >
+        Increase Number
+      </button>
+      <button
+        className="bg-blue-500 p-3 text-white cursor-pointer rounded-2xl hover:bg-blue-700 "
+        onClick={handleDecreaseClick}
+      >
+        Decrease Number
+      </button>
     </main>
   );
 }
