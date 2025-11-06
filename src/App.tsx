8eedc64b2b3021b776d3a228188f8ef148a678dc
Okoro Victor <okorov800@gmail.com>
2025-11-06 14:27:51 +0100

    2nd Commit: Products API fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import Products from "./Products";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const timerll = setTimeout(() => {
-      setCount(count + 3);
-    }, 2000);
-    return () => {
-      clearTimeout(timerll);
-    };
-  }, [count]);
-
   return (
     <main className="grid justify-items-center min-h-screen py-20 gap-5 bg-gray-00">
-      <h1>{count}</h1>
+      <Products />
     </main>
   );
 }
