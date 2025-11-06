a2997aa14443a61dd08ce9394de1e19fb26b564b
Okoro Victor <okorov800@gmail.com>
2025-11-06 14:01:59 +0100

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [index, setIndex] = useState(0);
+  const [count, setCount] = useState(0);
 
-  const handleIncreaseClick = () => {
-    setIndex(index + 1);
-    console.log(index);
-  };
-
-  const handleDecreaseClick = () => {
-    setIndex(index - 1);
-    console.log(index);
-  };
+  useEffect(() => {
+    const timerll = setTimeout(() => {
+      setCount(count + 3);
+    }, 2000);
+    return () => {
+      clearTimeout(timerll);
+    };
+  }, [count]);
 
   return (
     <main className="grid justify-items-center min-h-screen py-20 gap-5 bg-gray-00">
-      <h1 className="text-4xl">{index}</h1>
-      <button
-        className="bg-blue-500 p-3 text-white cursor-pointer rounded-2xl hover:bg-blue-700"
-        onClick={handleIncreaseClick}
-      >
-        Increase Number
-      </button>
-      <button
-        className="bg-blue-500 p-3 text-white cursor-pointer rounded-2xl hover:bg-blue-700 "
-        onClick={handleDecreaseClick}
-      >
-        Decrease Number
-      </button>
+      <h1>{count}</h1>
     </main>
   );
 }
