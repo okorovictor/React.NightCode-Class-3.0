0b10178df8be7e9baca4d16e6e12b2c85cb817f1
Okoro Victor <okorov800@gmail.com>
2025-11-18 17:23:50 +0100

    2nd commit: VideoPlayer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Form from "./Form";
+import VideoPlayer from "./VideoPlayer";
 
 function App() {
   return (
     <main className="grid justify-items-center min-h-screen py-20 gap-5 bg-gray-200">
       <Form />
+      <VideoPlayer />
     </main>
   );
 }
